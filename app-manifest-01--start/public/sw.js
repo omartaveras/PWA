f41c2153c8ec50d1b97f2902008f77ae0cb6ebea
omartaveras <omar.taverasdiaz@studenti.Unicam.it>
2020-07-29 18:33:24 +0100

    Pwa0.7 instaClone

---
+++ app-manifest-01--start/public/sw.js
@@ -0,0 +1,9 @@
+
+self.addEventListener('install', function(event){
+    console.log('[Service Worker]Installing Service Worker .. .', event);
+});
+
+self.addEventListener('install', function(event){
+    console.log('[Service Worker] Activating Service Worker .. .', event);
+    return self.clients.claim();
+});
