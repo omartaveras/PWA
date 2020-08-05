d83a60d3ad3b5486df153a801d904a4ac663c5f3
omartaveras <omar.taverasdiaz@studenti.unicam.it>
2020-08-05 18:20:23 +0100

    Pwa0.9 Fetch & Promise

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -18,7 +18,8 @@ window.addEventListener('beforeInstallPrompt', function(event){
 
 var promise = new Promise(function(resolve, reject){
     setTimeout(function(){
-    resolve('This is executed once the timer is done!');
+    //resolve('This is executed once the timer is done!');
+    reject({code:500, message: 'An error occurred!'})
     //console.log('This is executed once the timer is done!');
 }, 3000);
 });
@@ -31,6 +32,13 @@ var promise = new Promise(function(resolve, reject){
 //     console.log(newText);
 // });
 
-//prova
+promise.then(function(text){
+    return text;
+}).then(newText){
+    console.log(err.code, err.message);    
+}).catch(function(err){
+    console.log(err.code, err.message);
+});
+
 
 console.log('This is executed right after setTimeOut()...')
