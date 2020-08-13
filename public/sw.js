cb77510b88b74514d95da07836a7f28f9d86d89c
omartaveras <omar.taverasdiaz@studenti.unicam.it>
2020-08-13 18:51:11 +0100

    Pwa0.5.6 Cache & Promise

--- public/sw.js
+++ public/sw.js
@@ -5,7 +5,8 @@ self.addEventListener('install', function(event){
         caches.open('static')
         .then(function(cache){
             console.log('[Service Worker] Precaching App Shell');
-            cache.add('/src/js/app.js')
+            cache.add('/index.html');
+            cache.add('/src/js/app.js');
         })
     )
 });
